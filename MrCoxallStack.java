aaba7ded0740302d03508cb4dd04996a60f281fc
GavinZ0316 <31775654+GavinZ0316@users.noreply.github.com>
2018-04-26 12:23:36 -0400

    Create MrCoxallStack.java

---
+++ MrCoxallStack.java
@@ -0,0 +1,19 @@
+/**********************************************************************
+ * My Daily assingment2-01
+ * Created by: Gavin Zhou 
+ * Created on: Mar 2018
+ * This class create an integer stack
+ **********************************************************************/
+
+import java.util.*;
+
+
+public class MrCoxallStack{
+    ArrayList al = new ArrayList(); 
+     
+     public void push(String input){
+         al.add(input);
+         System.out.print(input);
+     } 
+
+}
